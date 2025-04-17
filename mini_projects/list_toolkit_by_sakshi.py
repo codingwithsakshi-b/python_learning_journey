423c3897ca1c9d209e6c761e1ae980357c5d62fc
Sakshi Sharma <studystudy7867@gmail.com>
2025-04-17 19:16:18 +0530

    Update list_toolkit_by_sakshi.py
    Signed-off-by: Sakshi Sharma <studystudy7867@gmail.com>

--- mini_projects/list_toolkit_by_sakshi.py
+++ mini_projects/list_toolkit_by_sakshi.py
@@ -26,5 +26,43 @@ def get_user_input():
         exit()
     return user_list
 
+def show_original_list(user_list):
+    #Displays the user's original list
+    print("\n🔁 Elements Recieved ✅")
+    print(f"↪️{user_list}")
 
+def covert_to_uppercase(user_list):
+    print("\n⬆️ Uppercase List is..")
+    upper_list = [items.upper() for items in user_list]
+    print(f"⬆️ Uppercase list: {upper_list}")
+    return upper_list
+
+def reverse_list(user_list):
+    print("\n🔄️ Reversed version:")
+    reverse = user_list[::-1]
+    print(f"🔄️ Reversed list: {reverse}")
+    return reverse
+
+def remove_item(user_list):
+    print("\n🚮 Removing an item from list...")
+    remove = input("⁉️ Enter an item you want to remove from list: ")
+    if remove in user_list:
+        user_list.remove(remove)
+        print(f"Updated list is: {user_list}")
+    else:
+        print("❌ {remove} is not found in list")
+
+def main():
+    print("🧺 Welcomme to Sakshi's List Toolkit!")
+    user_list = get_user_input()
+    show_original_list(user_list)
+    reverse_list(user_list)
+    convert_to_uppercase(user_list)
+    remove_item(user_list)
+    print("\n🌟 Thank You For using Sakshi's List Toolkit!")
+    print("💻 Created by Sakshi...")
+
+if __name__ == "__main__":
+    main()
+    
 
