c0dda23ba4569d92df91839991b59133eb9762f5
Sakshi Sharma <studystudy7867@gmail.com>
2025-04-17 16:07:24 +0530

    Update student_info_dict.py
    Signed-off-by: Sakshi Sharma <studystudy7867@gmail.com>

--- mini_projects/student_info_dict.py
+++ mini_projects/student_info_dict.py
@@ -1,43 +1,53 @@
 # 🎓 Welcome to Sakshi’s Student Info System 📘
 # 💡 Created with love by Sakshi | Polished by Aryan ✨
 
-my_dict = {}
+user_dict = {}
 
 print("🎓 Welcome to Sakshi’s Student Info System 📘")
 
 # 🔹 Input section
-name = input("\n📝 Enter Your Name: ").capitalize()
-age = int(input("📝 Enter Your Age: "))
-course = input("📝 Enter Your Course: ").upper()
+def collect_user_info():
+    name = input("\n📝 Enter Your Name: ").capitalize()
+    age = input("📝 Enter Your Age: ")
+    while not age.isdigit():
+        print("❌ Invalid Age Input. Please Enter a Valid Age.")
+        age = input("📝 Enter Your Age: ")
+    course = input("📝 Enter Your Course: ").upper()
+    return{"Name": name, "Age": int(age), "Course": course}
 
-# 👩‍🎓 Store initial data
-print("\n👩‍🎓 Original Data:")
-new_dict = {
-    "Name": name,
-    "Age": age,
-    "Course": course
-}
-my_dict.update(new_dict)
-print(my_dict)
+def add_university(user_dict):
+    #add use university to the user dictionary.
+    your_univ = input("🎓🏫 Enter Your University Name: ")
+    user_dict.update("University": your_univ)
+    print(f\n➕ Adding University = {your_univ}")
+    return user_dict
 
-# ➕ Add university info
-your_univ = input("\n🏫 Enter your University: ").upper()
-my_dict.update({"University": your_univ})
-print(f"\n➕ Adding University = {your_univ}")
+def remove_key_info(user_dict):
+    key_remove = input("Enter a KEY wants to remove from your dictionary: ")
+    if key_remove in user_dict:
+        user_dict.pop(key_remove)
+        print(f"❌ removing {key_remove} key...")
+    else:
+        print(f"{key_remove} key doesn't exist in dictionary")
+    return user_dict
 
-# ❌ Remove course info
-my_dict.pop("Course")
-print("❌ Removing 'Course' key...")
+def display_final_record(user_dict):
+    print("\n🔖 Final Student Record.")
+    print(user_dict)
+    print("\n📂 Keys:", user_dict.keys())
+    print("📜 Values:", user_dict.values())
 
-# 📦 Final output
-print("\n📦 Final Student Record:")
-print(my_dict)
+# Main Program
+if __name__ == "__main__":
+    print("🎓 Welcome to Sakshi’s Student Info System 📘")
+    user_dict = collect_user_info()
+    print("\n👩‍🎓 Original Data:")
+    print(user_dict)
 
-# 🔍 Show keys & values separately
-key = my_dict.keys()
-print(f"\n🗂️ Keys: {key}")
-value = my_dict.values()
-print(f"📄 Values: {value}")
+    user_dict = add_university_info(user_dict)
+    user_dict = remove_course_info(user_dict)
+    display_final_record(user_dict)
+
 
 # 🎉 Closing
 print("\n✨ Thank you for using Sakshi’s Smart Dictionary Tool!")
